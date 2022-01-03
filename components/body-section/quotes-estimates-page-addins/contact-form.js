c1eb25a9476a9c94f9690e4c1d96f9af3f471e5c
miketwheeler <mikeytwheeler@gmail.com>
2022-01-02 18:35:36 -0800

    fixed major issues with contact form fields, refactoring, added styles

--- components/body-section/quotes-estimates-page-addins/contact-form.js
+++ components/body-section/quotes-estimates-page-addins/contact-form.js
@@ -1,4 +1,4 @@
-import React, { useState, setState } from 'react'
+import React, { useState, useRef } from 'react'
 import { 
 	FormControl, 
 	FormControlLabel, 
@@ -6,11 +6,36 @@ import {
 	RadioGroup, 
 	TextField, 
 	Radio, 
-	Button 
+	Button,
 } from '@material-ui/core'
 import ContactFormStyles from './contact-form.module.css'
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
+// Styles applied to MUI form inputs - assists and fixes the native label incorrectly displaying
+const useStyles = makeStyles ({
+	label: {
+		backgroundColor: "white",
+		fontSize: '1.1rem',
+		border: '4px solid white',
+		marginTop: '-7px',
+	},
+	dateBox: {
+		marginBottom: '1ch',
+	},
+	dateWrapper: {
+		'&:focus': {
+			color: 'rgb(49, 49, 173)',
+		}
+	}
+}); 
+
+// Contact form Component for the Quotes page
 function ContactForm() {
+	const classes = useStyles();
+	const theme = useTheme();
+	const highlightRef = useRef(null);
+
+	// States for form inputs - passed to email handler
 	const [fullName, setFullName] = useState("");
 	const [phoneNumber, setPhoneNumber] = useState("");
 	const [email, setEmail] = useState("");
@@ -18,26 +43,13 @@ function ContactForm() {
 	const [dueBy, setDueBy] = useState("");
 	const [message, setMessage] = useState("");
 
+	// Special state for unadaptable component (date picker -> it's label)
+	const [isFocused, setIsFocused] = useState(false);
 
-	const handleFullNameChange = (event) => {
-		setFullName(event.target.value)
-	}
-	const handlePhoneNumberChange = (event) => {
-		setPhoneNumber(event.target.value)
-	}
-	const handleEmailChange = (event) => {
-		setEmail(event.target.value)
-	}
-	const handleRadioSelectionChange = (event) => {
-		setRadioSelectionValue(event.target.value);
-	}
-	const handleDueByChange = (event) => {
-		setDueBy(event.target.value);
-	}
-	const handleMessageChange = (event) => {
-		setMessage(event.target.value);
-	}
 
+/* ***************************************************************************************************** */
+//  if the new onChange assignments don't work as intended -- this is where the commented out section goes
+/* ***************************************************************************************************** */
 	const resetForm = () => {
 		setFullName("");
 		setPhoneNumber("");
@@ -53,52 +65,57 @@ function ContactForm() {
 		// TODO: Link below to the email dispatcher!!!!!                    ///////
 		// ////////////////////////////////////////////////////////////////////////
 		// sendEmail(fullName, phoneNummber, email, radioSelection, dueBy, message);
-		// build func to popup alert success/denial 
-		// alertBox();
+		// build func to popup alert success/denial ((validation - MUI has a section on validating))
+		// alertBox(); ???? or use above method for feedback - alert box maybe useful for submittion/denial of the form
 		resetForm();
 		console.log(fullName, phoneNumber, radioSelectionValue, dueBy, message, email);
 	}
 
 
 	return (
-		<form className={ContactFormStyles.form} onSubmit={submitMe}>
+		<form className={ContactFormStyles.form} noValidate autoComplete="off" onSubmit={submitMe}>
 			<TextField 
-				label="Full Name" 
+				InputLabelProps={{classes: {root: classes.label}}}
+				label="Name" 
 				variant="outlined"
 				fullWidth
+				type="text"
 				margin="normal"
 				autoComplete="true"
-				onChange={handleFullNameChange}
+				onChange={(e) => setFullName(e.target.value)}
 				value={fullName}
 				/>
 			<TextField 
+				InputLabelProps={{classes: {root: classes.label}}}
 				label="Phone Number" 
 				variant="outlined"
 				fullWidth
 				margin="normal"
 				autoComplete="true"
-				onChange={handlePhoneNumberChange}
+				onChange={(e) => setPhoneNumber(e.target.value)}
 				value={phoneNumber}
 				/>
 			<TextField 
+				InputLabelProps={{classes: {root: classes.label}}}
 				label="Email" 
 				variant="outlined"
 				width="500"
 				fullWidth
 				margin="normal"
 				autoComplete="true"
-				onChange={handleEmailChange}
+				onChange={(e) => setEmail(e.target.value)}
 				value={email}
 				/>
 			<FormControl aria-label="radio-list-form" margin="normal" fullWidth>
-				<FormLabel aria-label="subject-radio-list-selection">Subject</FormLabel>
+				<FormLabel aria-label="subject-radio-list-selection">Subject Matter</FormLabel>
 					<RadioGroup 
 						row 
 						aria-label="select-service-radio-buttons" 
 						defaultValue={radioSelectionValue}
 						name="currentSelection" 
 						value={radioSelectionValue} 
-						onChange={handleRadioSelectionChange}
+						onChange={(e) => setRadioSelectionValue(e.target.value)}
+						className={ContactFormStyles.radioContainer}
 						>
 						<FormControlLabel 
 							value="stairs" 
@@ -130,23 +147,46 @@ function ContactForm() {
 							/>
 					</RadioGroup>
 			</FormControl>
-			<TextField 
-				label="Due Date"
-				variant="outlined"
-				margin="normal" 
-				autoComplete="none"
-				onChange={handleDueByChange}
-				value={dueBy}
-				/>
+			<div className={classes.dateWrapper}>
+				<FormLabel 
+					id="#date-input" 
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						width: 'fit-content',
+						margin: '1ch 0 2ch',
+					}}
+					for="dateField"
+					focused={isFocused}
+					className={classes.inputLabelBox}
+					>
+					When Would You Like a Call Back?
+				</FormLabel>
+				<TextField
+					onFocus={() => setIsFocused(true)}
+					onBlur={() => setIsFocused(false)}
+					name="dateField"
+					id="#dateid"
+					type="date"
+					variant="outlined"
+					onChange={(e) => setDueBy(e.target.value)}
+					value={dueBy}
+					className={classes.dateBox}
+					/>
+			</div>
 			<TextField
-				label="My Message" 
+				InputLabelProps={{classes: {root: classes.label}}}
+				id="message"
+				label="My Message"
+				input='My Message'
+				type="text"
 				fullWidth 
 				variant="outlined"
 				margin="normal"
 				multiline 
 				rows={3} 
 				autoComplete="none"
-				onChange={handleMessageChange}
+				onChange={(e) => setMessage(e.target.value)}
 				value={message}
 				/>
 			<div className={ContactFormStyles.bttncase}>
@@ -163,4 +203,4 @@ function ContactForm() {
 	)
 }
 
-export default ContactForm
+export default ContactForm;
